d7f85174da60f25e5226f1baf1502ce858c23dd7
Ruchi <ruchi27tiwari@gmail.com>
2026-01-28 13:31:55 +0530

    Dynamic value

---
+++ Collection_programs/src/DynamicValue.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DynamicValue {
+    public static void main(String[]args){
+        String s;
+        Scanner sc= new Scanner(System.in);
+        s= sc.nextLine();
+        s = s.toLowerCase();
+
+        int count=0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'r') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+
